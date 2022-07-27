324464565baafda7d2fbd6487b5973584dba5151
Diego Coss <diegocoss@Diegos-MacBook-Air.local>
2022-07-27 13:25:23 -0700

    finishing single page

--- Components/SinglePage/SinglePage.js
+++ Components/SinglePage/SinglePage.js
@@ -1,13 +1,17 @@
 import { View, Text, Pressable, Image } from "react-native";
 import { styles } from "./SinglePageStyle.js";
+import { BottomDrawer } from 'react-native-bottom-drawer-view';
 
-export default function SinglePage() {
   return (
     <View style={styles.container}>
       <Text>TEST</Text>
-      <Pressable>
+      <Pressable onPress={() => console.log('Open Drawer')}>
         <Text>Press Me</Text>
       </Pressable>
+
+      <BottomDrawer containerHeight={100} offset={TAB_BAR_HEIGHT}>
+        {renderContent()}
+      </BottomDrawer>
     </View>
   );
 }
